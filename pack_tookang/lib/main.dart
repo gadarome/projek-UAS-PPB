53638430bea77a8a163ba0f589332bf4f57f1d76
Fajar-211 <21102183@ittelkom-pwt.ac.id>
2024-05-26 23:26:06 +0700

    tambahin

--- pack_tookang/lib/main.dart
+++ pack_tookang/lib/main.dart
@@ -1,44 +1,9 @@
-import 'package:flutter/material.dart';
-
-void main() async {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pack_tookang/firebase_options.dart';
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (context) => LoginCubit()),
-          BlocProvider(create: (context) => RegisterCubit())
-        ],
-        child: MaterialApp(
-          title: "Praktikum 6",
-          debugShowCheckedModeBanner: false,
-          navigatorKey: NAV_KEY,
-          onGenerateRoute: generateRoute,
-          home: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              } else if (snapshot.hasData) {
-                return HomeScreen();
-              } else if (snapshot.hasError) {
-                return const Center(
-                  child: Text('Something went wrong'),
-                );
-              } else {
-                return const LoginScreen();
-              }
-            },
-          ),
-        ));
-  }
 }
