badef7310eb8a7da663651ee062c935986ea2467
Fajar-211 <21102183@ittelkom-pwt.ac.id>
2024-06-01 23:05:08 +0700

    sprint-2

--- pack_tookang/lib/main.dart
+++ pack_tookang/lib/main.dart
@@ -1,9 +1,28 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pack_tookang/firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:pack_tookang/ui/splash.dart';
+import 'package:pack_tookang/ui/utils/routes.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pack_tookang",
+      debugShowCheckedModeBanner: false,
+      navigatorKey: NAV_KEY,
+      onGenerateRoute: generateRoute,
+      home: SplashScreen()
+    );
+  }
 }
